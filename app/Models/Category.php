66e11fc49591eab41bcfb77e31a1255b2da887ac
Ferry Noer Hendriawan <ferryentertaiment18@gmail.com>
2025-12-30 19:55:16 +0700

    Make models for databases

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'cat_name',
+        'description',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'order_code',
+        'user_id',
+        'subtotal',
+        'tax',
+        'grand_total',
+        'status',
+        'table_number',
+        'payment_method',
+        'note',
+        'created_at',
+        'updated_at'
+    ];
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
