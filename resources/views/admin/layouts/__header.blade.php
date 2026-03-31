2b8e8bd6016e837e4bb7ac2a24164eba7e271127
ryzecl <ferryentertaiment18@gmail.com>
2026-03-31 22:25:37 +0700

    feat: add admin header layout and authentication views with site assets

--- resources/views/admin/layouts/__header.blade.php
+++ resources/views/admin/layouts/__header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') || Restoranku</title>
 
-    <link rel="icon" href="https://apps.codepolitan.com/sites/learn/uploads/original/2/logo-codepolitan.png">
+    <link rel="icon" href="{{ asset('assets/burger.png') }}">
 
     <link rel="stylesheet" crossorigin href="{{ asset('assets/admin/compiled/css/app.css') }}">
     <link rel="stylesheet" crossorigin href="{{ asset('assets/admin/compiled/css/app-dark.css') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" crossorigin="" href="{{ asset('assets/admin/compiled/css/app.css') }}" />
     <link rel="stylesheet" crossorigin="" href="{{ asset('assets/admin/compiled/css/app-dark.css') }}" />
     <link rel="stylesheet" crossorigin="" href="{{ asset('assets/admin/compiled/css/auth.css') }}" />
-    <link rel="icon" href="https://apps.codepolitan.com/sites/learn/uploads/original/2/logo-codepolitan.png">
+    <link rel="icon" href="{{ asset('assets/burger.png') }}">
 </head>
 
 <body>
